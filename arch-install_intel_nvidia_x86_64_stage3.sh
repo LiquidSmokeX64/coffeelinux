9bf95b238ae010e25760b43edc2b462f8f6a46d2
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-03-31 16:40:05 -0700

    Updated for new repo

--- arch-install_intel_nvidia_x86_64_stage3.sh
+++ arch-install_intel_nvidia_x86_64_stage3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm arch-linux_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp /Guides-Scripts/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && umount -R /mnt && reboot
+rm /mnt/arch-linux_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp /arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && umount -R /mnt && chmod a+x /VAAPI-Chrome/chrome-flags.conf && reboot

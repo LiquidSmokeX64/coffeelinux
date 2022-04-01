c777b2fa0238ee2ec3feba04d040cb3cd230db32
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-04-01 20:05:33 +0000

    Delete arch-install_gnome_intel_nvidia_x86_64_stage4.sh

--- arch-install_gnome_intel_nvidia_x86_64_stage4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo git clone https://aur.archlinux.org/yay.git && sudo chown -R archuser:users ./yay && id archuser && cd yay && makepkg -si && cd && yay pamac-aur && yay libva-vdpau-driver-vp9-git && yay google-chrome && yay protontricks && yay chrome-gnome-shell && yay nvidia-vaapi-driver && exit

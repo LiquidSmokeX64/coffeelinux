a3076db6647f458fa91ca7efefa26d4c99a153b7
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-04-01 03:28:11 +0000

    Delete arch-install_intel_nvidia_x86_64_stage3.sh

--- arch-install_intel_nvidia_x86_64_stage3.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rm /mnt/arch-linux_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp /arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && umount -R /mnt && chmod a+x /VAAPI-Chrome/chrome-flags.conf && reboot

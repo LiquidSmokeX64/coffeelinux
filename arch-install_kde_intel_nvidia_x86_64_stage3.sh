5e84bcd548ebd8b75f978409ef9b61e3b8e43a16
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-04-01 05:06:09 +0000

    Update arch-install_kde_intel_nvidia_x86_64_stage3.sh

--- arch-install_kde_intel_nvidia_x86_64_stage3.sh
+++ arch-install_kde_intel_nvidia_x86_64_stage3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm /mnt/arch-linux_kde_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && umount -R /mnt && chmod a+x /mnt/VAAPI-Chrome/chrome-flags.conf && reboot
+rm /mnt/arch-instal_kde_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && chmod a+x /mnt/VAAPI-Chrome/chrome-flags.conf && umount -R /mnt && reboot

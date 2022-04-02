d3938724e5cb4b52560bb5bf25d1c400cdd348f6
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-04-02 16:38:02 -0700

    Upload Cinnamon DE

---
+++ arch-install_cinnamon_intel_nvidia_x86_64_stage4.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo git clone https://aur.archlinux.org/yay.git && sudo chown -R archuser:users ./yay && id archuser && cd yay && makepkg -si && cd && yay pamac-aur && yay libva-vdpau-driver-vp9-git && yay google-chrome && yay protontricks && yay chrome-gnome-shell && yay nvidia-vaapi-driver && yay cpupower-gui && yay mint-themes && yay mint-themes-legacy && yay mint-x-icons && yay mint-y-icons && yay mint-artwork && yay mint-backgrounds && yay mintlocale && yay blueberry && reboot

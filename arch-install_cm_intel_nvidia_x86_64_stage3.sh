21781db7df2787ff5b543fc66bb46a3684fa5726
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-04-02 02:27:38 +0000

    Add files via upload

---
+++ arch-install_cm_intel_nvidia_x86_64_stage3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo Cleaning up && rm /mnt/arch-install_cm_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && chmod a+x /mnt/VAAPI-Chrome/chrome-flags.conf && cp arch-linux-installer/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop /mnt/VAAPI-Chrome/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop && chmod a+x /mnt/VAAPI-Chrome/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop && umount -R /mnt && reboot

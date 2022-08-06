e785d97407a04cc040b2e8205550a14c3957714b
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-08-05 18:08:24 -0700

    Delete arch-install_xfce_intel_nvidia_x86_64_stage4.sh

--- arch-install_xfce_intel_nvidia_x86_64_stage4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo git clone https://aur.archlinux.org/yay.git && sudo chown -R archuser:users ./yay && id archuser && cd yay && makepkg -si && cd && yay pamac-aur && yay libva-vdpau-driver-vp9-git && yay google-chrome && yay protontricks && yay nvidia-vaapi-driver && yay cpupower-gui && yay mint-themes && yay mint-themes-legacy && yay mint-x-icons && yay mint-y-icons && yay mint-backgrounds && xfconf-query -c xfwm4 -p /general/vblank_mode -s glx && reboot

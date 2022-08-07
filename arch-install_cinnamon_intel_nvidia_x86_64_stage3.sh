5105e995e24125967fa9f08fe4270af6e5958bb8
LiquidSmokeX64 <dlc07projectxmods@gmail.com>
2022-08-06 23:58:14 -0700

    Changed to new scripting format.

--- arch-install_cinnamon_intel_nvidia_x86_64_stage3.sh
+++ arch-install_cinnamon_intel_nvidia_x86_64_stage3.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-echo Cleaning up && rm /mnt/arch-install_cinnamon_intel_nvidia_x86_64_stage2.sh && mkdir /mnt/VAAPI-Chrome && cp arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && chmod a+x /mnt/VAAPI-Chrome/chrome-flags.conf && cp arch-linux-installer/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop /mnt/VAAPI-Chrome/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop && chmod a+x /mnt/VAAPI-Chrome/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop && umount -R /mnt && reboot
+echo 'Cleaning up' && 
+rm /mnt/arch-install_cinnamon_intel_nvidia_x86_64_stage2.sh && 
+mkdir /mnt/VAAPI-Chrome && 
+cp arch-linux-installer/chrome-flags.conf /mnt/VAAPI-Chrome/chrome-flags.conf && 
+chmod a+x /mnt/VAAPI-Chrome/chrome-flags.conf && 
+cp arch-linux-installer/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop /mnt/VAAPI-Chrome/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop && 
+chmod a+x /mnt/VAAPI-Chrome/chrome-pnkcfpnngfokcnnijgkllghjlhkailce-Default.desktop && 
+umount -R /mnt && reboot
